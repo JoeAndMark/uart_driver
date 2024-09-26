12ec102bc3ff27abdb34065119806aecfffa24dc
JoeAndMark <858446559@qq.com>
2024-09-26 20:48:47 +0800

    [ feature]: 增加获取 Uart 属性的接口
    Uart 类中所有属性全部私有，增加外部访问接口，保证开发者能够随时查看属性，并保证安全。
    增加的接口如下所示：
      - `getPort()`    : 获取串口设备路径
      - `getBaudRate()`: 获取波特率
      - `getFd()`      : 获取串口的文件描述符
      - `getHfcState()`: 获取硬件流控制状态，即是否打开硬件流控制
      - `getSfcState()`: 获取软件流控制状态，即是否打开软件流控制
      - `getStopBits()`: 获取停止位数

--- uart.hpp
+++ uart.hpp
@@ -2,7 +2,7 @@
 #define __UART_HPP
 
 // 标准库
-
+#include <iostream>
 
 // 第三方库
 #include <termios.h>
@@ -36,6 +36,52 @@ public:
     
     ~Uart() {
 
+    }
+
+    /**
+     * @brief 获取串口设备路径
+     */
+    const char* getPort() const {
+        return _port;
+    }
+
+    /**
+     * @brief 获取波特率
+     */
+    int getBaudRate() const {
+        return _baudRate;
+    }
+
+    /**
+     * @brief 获取当前设备的文件描述符
+     * @return 返回文件描述符
+     */
+    int getFd() const {
+        return _fd;
+    } /* int getFd() const { */
+
+    /**
+     * @brief 获取硬件流控制状态
+     * @return true表示开启硬件流控制，反之表示关闭硬件流控制
+     */
+    bool getHfcState() const {
+        return _hfc;
+    }
+
+    /**
+     * @brief 获取软件流控制状态
+     * @return true表示开启软件流控制，反之表示关闭软件流控制
+     */
+    bool getSfcState() const {
+        return _sfc;
+    }
+
+    /**
+     * @brief 获取停止位数
+     * @return 返回停止位数
+     */
+    int getStopBits() const {
+        return _stopBits;
     }
 
 private:
